a4a03a458dc97ac5d0f46ece8d6b03305bae4662
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-30 22:35:22 +0000

    populate the db in the background during launch

--- app/src/main/java/me/tokornoe/roomwordsample/WordRoomDatabase.kt
+++ app/src/main/java/me/tokornoe/roomwordsample/WordRoomDatabase.kt
@@ -1,9 +1,13 @@
 package me.tokornoe.roomwordsample
 
 import android.content.Context
+import androidx.annotation.RestrictTo
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 // Annotates class to be a Room Database with a table (entity) of the Word class
 @Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
@@ -15,7 +19,7 @@ abstract class WordRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             val tempInstance = INSTANCE
             if (tempInstance != null) {
                 return tempInstance
@@ -25,10 +29,30 @@ abstract class WordRoomDatabase : RoomDatabase() {
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build()
+                ).addCallback(WordDatabaseCallback(scope)).build()
                 INSTANCE = instance
                 return instance
             }
+        }
+    }
+
+    private class WordDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback() {
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            //Delete all content here
+            //wordDao.deleteAll()
+
+            // Add sample words
+            var word = Word("Kelvin")
+            wordDao.insert(word)
         }
     }
 }
